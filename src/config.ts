33eca5d1fe886f223217c54be175def614c44604
Luka Petrović <refr4g@outlook.com>
2024-11-15 00:03:33 +0100

    Updates...

--- src/config.ts
+++ src/config.ts
@@ -8,11 +8,11 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'refr4g',
-  subtitle: 'refr4g\'s blog',
+  subtitle: 'infosec blog',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 175,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false,     // Hide the theme color picker for visitors
+    fixed: true,     // Hide the theme color picker for visitors
   },
   banner: {
     enable: true,
