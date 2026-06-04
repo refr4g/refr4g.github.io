7a8300bfb23bd419a0dc7bec9115c06f3a62b2d6
Luka Petrovic <63981656+refr4g@users.noreply.github.com>
2026-06-04 21:17:30 +0200

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://refr4g.github.io/",
+  site: "https://attacke.rs/",
   base: "/",
   trailingSlash: "always",
   integrations: [
